0e7bb9269dd2cdbc880d69f1aa9d207a56ce1c73
youjheng <chagam0507@gmail.com>
2022-07-07 05:26:44 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Url = require('./models/url')
 
 const app = express()
 const PORT = 3000
--- models/url.js
+++ models/url.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const urlSchema = new Schema({
-  trueUrl: {
+  originalUrl: {
     type: String,
     required: true,
   },
