26f5549bd95b34479bb7798857cf1ad8c6f6caa3
youjheng <chagam0507@gmail.com>
2022-07-09 15:37:28 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const PORT = 3000
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(routes)
